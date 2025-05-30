b9361cabf4648d777be9e5d6a5ab6c19014f12d8
MarrVL <mariel.valdez@tecsup.edu.pe>
2025-05-30 09:12:11 -0500

    Ultimo commit terrminado

--- app/src/main/java/com/example/moviecounter/MainActivity.kt
+++ app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.moviecounter.ui.theme.MovieCounterTheme
+import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,29 +31,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MovieCounter(modifier: Modifier = Modifier) {
-    var count by remember { mutableStateOf(0) }
-    var movieName by remember { mutableStateOf("") }
+    var count by rememberSaveable { mutableStateOf(0) }
+    var movieName by rememberSaveable { mutableStateOf("") }
+    var message by rememberSaveable { mutableStateOf("") }
 
     Column(
-        modifier = modifier.padding(16.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "You have added $count movies.")
+        Text(text = "Registro de Películas.",
+             style = MaterialTheme.typography.headlineSmall)
         Spacer(modifier = Modifier.height(16.dp))
         TextField(
             value = movieName,
             onValueChange = { movieName = it },
-            label = { Text("Movie Name") }
+            label = { Text("Movie Name") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = {
             if (movieName.isNotBlank()) {
                 count++
+                message = "Se agregó \"$movieName\". Total: $count"
                 movieName = ""
+            } else {
+                message = "Por favor, ingresa un nombre de película."
             }
         }) {
             Text("Add Movie")
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = message)
     }
 }
 
